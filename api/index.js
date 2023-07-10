523c88d8e7e514cf3a0997910a0e6b0beb7eae5d
Jonathan Lourie <65506981+jonlourie@users.noreply.github.com>
2023-07-10 19:45:27 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use('/uploads', express.static(__dirname+'/uploads'));
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: 'https://publockapp-268aj0cqy-jonlourie.vercel.app',
 }));
 
 mongoose.connect(process.env.MONGO_URL);
